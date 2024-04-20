b40908f73a9fc0d50d5c2ef245dab112bda2c150
Lenka Zufala <m-lenkazufala@seznam.cz>
2024-04-20 19:28:35 +0200

    git commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let hodinovka = 3000
+let pocetHodin = 160
+let vyplata = hodinovka * pocetHodin
+
+document.body.innerHTML = vyplata
+
+let start = 15
+let delka = 13
+let konec = start + delka - 24
+document.body.innerHTML = konec
+ 
+document.body.innerHTML += "<p>" + Math.round (Math.random()*10) + "</p>"
+
+let wageInEur = 20
+let Eur = 24.55
+
+var wageInCzk = wageInEur * Eur
+document.body.innerHTML = wageInCzk
